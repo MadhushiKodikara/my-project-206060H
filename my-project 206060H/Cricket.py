ad25d2d319c0e6a67db560bba8367064a39d82ef
Kodikaramd.20 <madhushikodi@gmail.com>
2023-10-15 11:42:19 +0530

    Cricket.py

---
+++ my-project 206060H/Cricket.py
@@ -0,0 +1,33 @@
+import random
+
+# Simulated win-loss data for Sri Lanka (you should replace this with real data)
+sri_lanka_wins = 150
+sri_lanka_losses = 100
+
+# Function to simulate a match result
+def simulate_match():
+    return random.choice(["Sri Lanka", "Opponent"])
+
+# Number of simulations to run
+num_simulations = 10000
+
+# Initialize counters for Sri Lanka's wins and losses
+sri_lanka_wins_count = 0
+sri_lanka_losses_count = 0
+
+# Simulate matches and count wins and losses
+for _ in range(num_simulations):
+    winner = simulate_match()
+    if winner == "Sri Lanka":
+        sri_lanka_wins_count += 1
+    else:
+        sri_lanka_losses_count += 1
+
+# Calculate the probability of Sri Lanka winning
+probability_sri_lanka_wins = sri_lanka_wins_count / num_simulations
+
+# Calculate the probability of Sri Lanka losing
+probability_sri_lanka_losses = sri_lanka_losses_count / num_simulations
+
+print(f"Probability of Sri Lanka winning: {probability_sri_lanka_wins:.2%}")
+print(f"Probability of Sri Lanka losing: {probability_sri_lanka_losses:.2%}")
